29e2feda82f10fb54dc68fe7d726495a17bf2707
Nikita <tyumin000@gmail.com>
2024-11-01 15:04:42 +0300

    feat: add config

---
+++ detection/internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+)
+
+type Config struct {
+	Env   string      `yaml:"env"`
+	Port  int         `yaml:"port"`
+	Redis RedisConfig `yaml:"redis"`
+	Kafka KafkaConfig `yaml:"kafka"`
+}
+
+type RedisConfig struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+	Pass string `yaml:"pass"`
+}
+
+type KafkaConfig struct {
+	Host  string `yaml:"host"`
+	Port  int    `yaml:"port"`
+	Topic string `yaml:"topic"`
+}
+
+// MustLoadPath loads config from configPath and panics on any errors
+func MustLoadPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic(fmt.Errorf("config file does not exist: %s", configPath))
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		panic(fmt.Errorf("failed to read config from %s: %s", configPath, err.Error()))
+	}
+
+	return &cfg
+}
